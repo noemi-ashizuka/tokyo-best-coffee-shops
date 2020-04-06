4314e9974a8b2bc845fbbc15baf4136f685bf264
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-06 14:27:08 +0900

    added show method and view

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -2,4 +2,9 @@ class CoffeeShopsController < ApplicationController
   def index
     @coffee_shops = policy_scope(CoffeeShop)
   end
+
+  def show
+    @coffee_shop = CoffeeShop.find(params[:id])
+    authorize @coffee_shop
+  end
 end
