f994bd487849cb0faed51b4a934a356ee30010a2
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-11 17:38:39 +0900

    added responsiveness to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#index'
+  root to: 'coffee_shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   namespace :api, defaults: { format: :json } do
