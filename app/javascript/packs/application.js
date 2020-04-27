4e41d528c8ac1a2bcacefe62cd45be0c840604bc
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-27 17:21:35 +0900

    added stars rating to review form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initStarRating } from '../plugins/init_star_rating';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
@@ -37,3 +38,5 @@ document.addEventListener('turbolinks:load', () => {
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
 })
+
+initStarRating();
