81f1fe7a0844e1de584c01722f621110384e50e7
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-27 12:10:11 +0900

    configured mapbox lookup

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -1,10 +1,10 @@
 class CoffeeShop < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_one_attached :photo
   has_one_attached :url_image
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, :reverse_geocode, if: :will_save_change_to_address?
   before_validation :geocode, on: %i[create update]
 
   validates :name, :address, presence: true
