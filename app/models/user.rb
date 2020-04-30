d47adc96ef122628776598d034c721299a1de489
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-30 13:16:23 +0900

    added blank avatar default

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :coffee_shops
+  has_many :reviews, dependent: :destroy
+
   has_one_attached :photo
+
+  def my_shops
+    CoffeeShop.where(user_id: id)
+  end
 end
